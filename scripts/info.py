eb1dd2413c2bea5b1933bf4cb380cccf262d668b
deresmos <deresmos@gmail.com>
2017-08-29 00:14:24 +0900

    Fix info script

--- scripts/info.py
+++ scripts/info.py
@@ -3,46 +3,47 @@ import os
 
 import print_cal
 from common import *
+from common import ConkyConfWriter
 
 
-def config():  # {{{1
-    str = ''
+class InfoConkyConf(ConkyConfWriter):
+    def __init__(self):  # {{{2
+        super().__init__('info.conf')
 
-    return str + '}\n\n'
+    def get_conf(self):  # {{{2
+        text = '''
+            ${voffset -8}$alignr$color${font Bitstream Vera Sans:size=30}${time %Y}$font
+            $color${voffset -30}$color${font Bitstream Vera Sans:size=20}${time %m}$font\
+            ${voffset -3} $color${font Bitstream Vera Sans:size=22}${time %d}$font$color2$hr
+            $alignc$color${font :size=36}${time %H}:${time %M}
+            '''
 
+        text += self.h2('INFORMATION')
+        text += '''
+            ${goto 40}${color2}HDD FREE:$alignr$color${fs_free} / ${fs_size}
+            ${goto 40}${color2}HDD:$color$fs_used_perc% ${fs_bar 6}
 
-def generate_info_conf():  # {{{1
-    str = config()
+            ${voffset 10}${goto 40}${color}Swap${font Bitstream Vera Sans:bold:size=8}$alignr${swap}/ ${swapfree}
+            ${voffset 15}$font$alignr${execi 10000 awk -F= '/TION/ {print $2}' /etc/lsb-release |sed 's/"//g'} \
+            ${execi 10000 awk -F= '/EASE=/ {printf $2" "} /NAME/ {print $2}' /etc/lsb-release}
+            ${voffset 10}${color2}${alignr}${execi 1200 whoami}@${nodename}
+            ${alignr}${color2}${font Bitstream Vera Sans:size=8}uptime: ${color}${uptime_short}
+            ${voffset 5}${color2}${font Bitstream Vera Sans:size=8}${alignr}kernel: ${color}${kernel}
+            '''
 
-    str += 'conky.text = [[\n\n'
+        text += self.h2('Calendar')
+        text += print_cal.createCal()
 
-    str += '''
-${voffset -8}$alignr$color${font Bitstream Vera Sans:size=30}${time %Y}$font
-$color${voffset -30}$color${font Bitstream Vera Sans:size=20}${time %m}$font\
-${voffset -3} $color${font Bitstream Vera Sans:size=22}${time %d}$font$color2$hr
-$alignc$color${font :size=36}${time %H}:${time %M}
-'''
-    str += h2('INFORMATION')
-    str += '''
-${goto 40}${color2}HDD FREE:$alignr$color${fs_free} / ${fs_size}
-${goto 40}${color2}HDD:$color$fs_used_perc% ${fs_bar 6}
+        return self._get_conf(text)
 
-${voffset 10}${goto 40}${color}Swap${font Bitstream Vera Sans:bold:size=8}$alignr${swap}/ ${swapfree}
-${voffset 15}$font$alignr${execi 10000 awk -F= '/TION/ {print $2}' /etc/lsb-release |sed 's/"//g'} \
-${execi 10000 awk -F= '/EASE=/ {printf $2" "} /NAME/ {print $2}' /etc/lsb-release}
-${voffset 10}${color2}${alignr}${execi 1200 whoami}@${nodename}
-${alignr}${color2}${font Bitstream Vera Sans:size=8}uptime: ${color}${uptime_short}
-${voffset 5}${color2}${font Bitstream Vera Sans:size=8}${alignr}kernel: ${color}${kernel}
-'''
+    def get_config(self):  # {{{2
+        return self._get_config('')
 
-    str += h2('Calendar')
-    str += print_cal.createCal()
-
-    str += '\n]]\n'
-    return str
+    def save_conf(self):  # {{{2
+        with open(self._conf_filename, 'w') as f:
+            f.write(self.get_conf())
 
 
 if __name__ == '__main__':  # {{{1
-    import common
-    with open(os.path.join(common.config_dir, 'info.conf'), 'w') as f:
-        f.write(generate_info_conf())
+    text = InfoConkyConf()
+    print(text.get_conf())
